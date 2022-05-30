d1789b61f976279fa98f54e4fff6d40c03635f87
fvarrin <florian.varrin@gmail.com>
2022-05-30 18:01:39 +0200

    Add output in parsing

--- srcs/parser/io.c
+++ srcs/parser/io.c
@@ -6,7 +6,7 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 17:02:35 by fvarrin           #+#    #+#             */
-/*   Updated: 2022/05/30 16:06:08 by fvarrin          ###   ########.fr       */
+/*   Updated: 2022/05/30 18:00:44 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 #include "libft.h"
 #include "minishell.h"
 
-char	**get_input_from_begining(char **arguments, t_command *command)
+char	**get_io_from_begining(char **arguments, t_command *command)
 {
 	int		i;
 	int		args_begin;
 
+	if (ft_strcmp(arguments[0], "<") != 0)
+		return (arguments);
 	command->in = ft_strdup(arguments[1]);
 	i = 2;
 	args_begin = i;
@@ -31,18 +33,24 @@ char	**get_input_from_begining(char **arguments, t_command *command)
 	return (&arguments[args_begin]);
 }
 
-char	**get_input_from_end(char **arguments, t_command *command)
+char	**get_io_from_end(char **arguments, t_command *command)
 {
 	int		i;
 
 	i = 0;
-	while (arguments[i] && ft_strcmp(arguments[i], "<") != 0)
+	while (arguments[i] &&
+		ft_strcmp(arguments[i], "<") != 0 && ft_strcmp(arguments[i], ">") != 0)
 		i++;
 	if (arguments[i] == NULL)
 		return (arguments);
-	command->in = ft_strdup(arguments[i + 1]);
+	if (ft_strcmp(arguments[i], ">") == 0)
+		command->out = ft_strdup(arguments[i + 1]);
+	else
+		command->in = ft_strdup(arguments[i + 1]);
 	while (arguments[i])
 	{
+		if (ft_strcmp(arguments[i], ">") == 0)
+			command->out = ft_strdup(arguments[i + 1]);
 		free(arguments[i]);
 		arguments[i] = NULL;
 		i++;
@@ -50,9 +58,10 @@ char	**get_input_from_end(char **arguments, t_command *command)
 	return (arguments);
 }
 
+
 char	**get_io_for_command(char **arguments, t_command *command)
 {
-	if (ft_strcmp(arguments[0], "<") == 0)
-		return (get_input_from_begining(arguments, command));
-	return (get_input_from_end(arguments, command));
+	arguments = get_io_from_begining(arguments, command);
+	arguments = get_io_from_end(arguments, command);
+	return (arguments);
 }
