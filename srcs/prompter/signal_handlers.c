92b14d887e3a52306613f79e3fc12a0653486968
fvarrin <florian.varrin@gmail.com>
2022-06-11 14:45:34 +0200

    Fix styling

--- srcs/prompter/signal_handlers.c
+++ srcs/prompter/signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: kmendes <kmendes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 00:00:06 by kmendes           #+#    #+#             */
-/*   Updated: 2022/06/08 00:00:11 by kmendes          ###   ########.fr       */
+/*   Updated: 2022/06/11 14:44:04 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
 #include <readline/readline.h>
 #include <unistd.h>
 
-/*
+/**
  * disable print of control characters
-*/
+ */
 void	configure_termios(void)
 {
 	static struct termios	term;
@@ -29,10 +29,12 @@ void	configure_termios(void)
 	tcsetattr(0, 0, &term);
 }
 
-/*
+/**
  * SIGINT parent handler
  * can lead to race conditions, see set_parent_signals
-*/
+ *
+ * @param {int} status
+ */
 static void	sigint_handler(int status __attribute__((unused)))
 {
 	write(1, "\n", 1);
@@ -41,13 +43,13 @@ static void	sigint_handler(int status __attribute__((unused)))
 	rl_redisplay();
 }
 
-/*
- * SIGQUIT ignored 
+/**
+ * SIGQUIT ignored
  * SIGINT signaction, block all signals for prevent race conditions
  *  see sigint_handler
  * redudant function, SIGINT is the only who need to be restored
  * call it at parent's start and after execute_plan
-*/
+ */
 void	set_parent_signals(void)
 {
 	static struct sigaction	act_sigint = {.sa_handler = sigint_handler};
@@ -57,20 +59,20 @@ void	set_parent_signals(void)
 	sigaction(SIGINT, &act_sigint, NULL);
 }
 
-/*
+/**
  * SIGINT ignored, use it before fork
  * This prevent SIGINT being catch by parent during child's execution
  * call it before execute_plan
-*/
+**/
 void	unset_parent_signals(void)
 {
 	signal(SIGINT, SIG_IGN);
 }
 
-/*
+/**
  * Restore default state SIGQUIT, SIGINT for child's execution
  * call it after fork in child's process
-*/
+**/
 void	set_child_signals(void)
 {
 	signal(SIGQUIT, SIG_DFL);
