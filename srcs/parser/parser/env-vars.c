0a56c68cb52de878267d958cf58ee8437255ffd8
fvarrin <florian.varrin@gmail.com>
2022-10-02 12:11:01 +0200

    #43 - Remove env variable leak

--- srcs/parser/parser/env-vars.c
+++ srcs/parser/parser/env-vars.c
@@ -6,7 +6,7 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 14:41:43 by fvarrin           #+#    #+#             */
-/*   Updated: 2022/09/25 16:00:45 by fvarrin          ###   ########.fr       */
+/*   Updated: 2022/10/02 12:09:44 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ t_env_variable	*get_env_variable(t_list_el *env, char *token_value)
 	t_env_variable	*env_variable;
 
 	env_variable = malloc(sizeof(t_env_variable));
+	if (env_variable == NULL)
+		return (NULL);
 	env_variable->token_value = token_value;
 	env_variable->token_value_length = ft_strlen(token_value);
 	env_variable->key = extract_env_variable_key_from_str(token_value);
@@ -34,6 +36,16 @@ t_env_variable	*get_env_variable(t_list_el *env, char *token_value)
 	env_variable->value = get_env_value(env, env_variable->key);
 	env_variable->value_length = ft_strlen(env_variable->value);
 	return (env_variable);
+}
+
+t_env_variable *destroy_env_variable(t_env_variable *env_variable)
+{
+	if (env_variable == NULL)
+		return (NULL);
+	free(env_variable->key);
+	free(env_variable->value);
+	free(env_variable);
+	return (NULL);
 }
 
 /**
@@ -52,6 +64,8 @@ void	expend_env_variable(t_list_el *env, t_token *token)
 	t_env_variable	*env_variable;
 
 	env_variable = get_env_variable(env, token->value);
+	if (env_variable == NULL)
+		return ;
 	expanded_value = ft_calloc(
 			calculate_env_variable_expanded_length(env_variable), sizeof(char));
 	i = 0;
@@ -64,6 +78,7 @@ void	expend_env_variable(t_list_el *env, t_token *token)
 	i += (env_variable->key_length + 1);
 	while (token->value[i])
 		expanded_value[y++] = token->value[i++];
+	destroy_env_variable(env_variable);
 	free(token->value);
 	token->value = expanded_value;
 }
